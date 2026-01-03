bb269db58cfd7ee29409f40205008b6f325a8240
Trinh Quoc Khanh <trinhquockhanh20121999@gmail.com>
2026-01-03 15:44:42 +0700

    Add CLI class for command line interface functionality

---
+++ ui/CLI.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "common/typedef.h"
+#include "UI.h"
+class CLI : public UI
+{
+public:
+    CLI(AppContext& context);
+    ~CLI() override {}
+    void onChatView_ShowMainMenu(const std::vector<ChatOption>& options) override;
+    void onShowError(const std::string& errorMessage) override;
+    void onShowInfo(const std::string& infoMessage) override;
+    int exec() override;
+
+private:
+    AppContext& context_;
+};
